ddd35a78aa2c67f13dcf1a0bb3b5ce947fb5e907
Matheus Pedroni <pnmatheus@protonmail.com>
2022-10-27 20:46:09 -0300

    nvim config files

---
+++ nvim/lua/mysettings/packer.lua
@@ -0,0 +1,35 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  } 
+  use 'numToStr/Comment.nvim' 
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'rafamadriz/friendly-snippets'
+  use 'akinsho/bufferline.nvim'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'sbdchd/neoformat'
+  use 'lewis6991/gitsigns.nvim'
+  use 'ray-x/lsp_signature.nvim'
+  use {
+    "folke/trouble.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true }
+  }
+  use "folke/trouble.nvim"
+end)
