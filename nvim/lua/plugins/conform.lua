3425c34d476510b2a8120d27458732e7b96df4d5
Matheus Pedroni <pnmatheus@protonmail.com>
2023-10-12 23:35:43 -0300

    new config for Go

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({
+          group = augroup,
+          buffer = bufnr,
+        })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
+      end
+    end,
+  }
+}
