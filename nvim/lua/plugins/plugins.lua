54ad77e57b37f1274d36f81a803ca62588474626
Matheus Pedroni <pnmatheus@protonmail.com>
2024-12-03 17:23:35 -0300

    go neovim config

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -3,10 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
         "toml",
         "elixir",
         "r",
@@ -18,74 +14,9 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
-        "gopls",
-        "gomodifytags",
-        "impl",
-        "goimports",
-        "gofumpt",
         "delve",
-        "staticcheck",
         "elixir-ls",
       })
     end,
-  },
-  {
-    "olexsmir/gopher.nvim",
-    lazy = true,
-    ft = { "go", "gomod" },
-    requires = { -- dependencies
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    build = function()
-      vim.cmd([[silen! GoInstallDeps]])
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        go = { "goimports", "gofumpt" },
-      },
-    },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.diagnostics.staticcheck,
-        nls.builtins.code_actions.gomodifytags,
-        nls.builtins.code_actions.impl,
-        nls.builtins.formatting.goimports,
-        nls.builtins.formatting.gofumpt,
-      })
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    dependencies = {
-      {
-        "leoluz/nvim-dap-go",
-        config = true,
-      },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    dependencies = {
-      "nvim-neotest/neotest-go",
-    },
-    opts = {
-      adapters = {
-        ["neotest-go"] = {
-          -- Here we can set options for neotest-go, e.g.
-          -- args = { "-tags=integration" }
-          recursive_run = true,
-        },
-      },
-    },
   },
 }
