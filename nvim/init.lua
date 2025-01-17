739e48daafb7139109886677d6cc74c756439e65
Matheus Pedroni <pnmatheus@protonmail.com>
2025-01-16 21:56:49 -0300

    avante setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
+local env_file = vim.fn.expand("~/.config/nvim/.env")
+if vim.fn.filereadable(env_file) == 1 then
+  for line in io.lines(env_file) do
+    local key, value = line:match("^([%w_]+)=([^\n]+)$")
+    if key and value then
+      vim.env[key] = value
+    end
+  end
+end
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
